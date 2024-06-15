ade3f99a5ebf3c7095a390e0a1e5c14d2dc58498
ewinrahman <rahmanewin@gmail.com>
2024-06-15 12:53:36 +0700

    second development fow gws logs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+import json
 import time
 
 ''' Raw log splitter information/template (Important fields only):
@@ -21,26 +22,11 @@ ref:
 - Threat: https://docs.paloaltonetworks.com/pan-os/9-1/pan-os-admin/monitoring/use-syslog-for-monitoring/syslog-field-descriptions/threat-log-fields#id83052cb2-4798-4f9c-abf8-e0b929ce7a3b
 '''
 
-'''Log example'''
-t = 'Jun  5 19:27:48 Panorama.nogle.com 1,2024/06/05 19:27:48,016201041583,TRAFFIC,end,2562,2024/06/05 19:27:40,165.154.129.130,123.51.190.204,165.154.129.130,10.1.31.77,BI-NAT-Internal System_Style.me Gitlab,,,ssl,vsys1,sdwan,vlan31,ethernet1/2,vlan.31,panorama,2024/06/05 19:27:40,1009045,1,34206,443,34206,443,0x400c1a,tcp,allow,4154,739,3415,11,2024/06/05 19:27:25,1,allurl,,7358049596824040546,0x8000000000000000,United Kingdom,Taiwan,,6,5,tcp-rst-from-client,11,0,0,0,,Xinyi-3220,from-policy,,,0,,0,,N/A,0,0,0,0,4b99d56e-8f04-4ef4-8567-03a392f02d9e,0,0,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,2024-06-05T19:27:41.583+08:00,,,encrypted-tunnel,networking,browser-based,4,"used-by-malware,able-to-transfer-file,has-known-vulnerability,tunnel-other-application,pervasive-use",,ssl,no,no,0'
+import re
+import time
 
-log = ''
-
-'''Parser Logic'''
-def paloalto_traffic(log):
-    delimiter = '\,'
-
-    parse = re.split(delimiter, {log})
-    parsedlogslist = []
-    try:
-        parsedlogs = list(filter(None, parse))
-        parsedlogslist.append(parsedlogs)
-        print(parsedlogslist[0])
-    except:
-        pass
-
-'''Menu Options'''
-def menu_def():
+'''Menu & Sub-Menu Functions'''
+def main_menu_def():
     menu_options = ('s', 'h', 'x')
 
     while True:
@@ -48,50 +34,106 @@ def menu_def():
         print('s = start')
         print('h = help')
         print('x = exit')
-
         print()
+        
         user_input = input('Enter an option: ')
 
         if user_input.lower() in menu_options:
-            break
+            return user_input
         else:
             print('Option is not in the menu!')
 
-    while True:
-        if user_input == 's':
-            print('What log you want to parse?')
-            parsing_option = (
-                'pa','gws','cs','x'
-            )
-            start_input = input('Choose log type: ')
+def parser_menu_def(user_input):
+    if user_input == 's':
+        print('What log do you want to parse?')
+        parsing_options = ('pa', 'gws', 'cs', 'x')
         
-            if start_input.lower() in parsing_option:
-                if start_input == 'pa':
-                    pa_log = input('paste the log here: ')
-                    paloalto_traffic(pa_log)
-                elif start_input == '':
-                    print('Log type is not supported')
-        if user_input == 'h':
-            print('Supported logs:')
-            print('pa = Palo Alto - Traffic log')
-            print('gws = Google Workspace - Google Drive Report')
-            print('x = Cancel')
-            print()
-            
-        if user_input == 'x':
-            exit()
-    # while True:
-    #     if user_input == 'pa':
-    #         log = input('Paste the log here: ')
-    #         paloalto_traffic()
-    #         break
-            
-menu_def()
+        while True:
+            start_input = input('Choose log type: ')
+            if start_input.lower() in parsing_options:
+                return start_input
+            else:
+                print('Log type is not supported')
+                
+    elif user_input == 'h':
+        print('Supported logs:')
+        print('pa = Palo Alto - Traffic log')
+        print('gws = Google Workspace - Google Drive Report')
+        print('cs = Custom log')
+        print('x = Cancel')
+        print()
+        
+    elif user_input == 'x':
+        exit()
+
+def parser_submenu_def(log_type):
+    if log_type in ('pa', 'gws', 'cs'):
+        logs = input('Paste the log here: '
+                     )
+        return logs
+    elif log_type == 'x':
+        exit()
+    else:
+        print('Log type is not supported')
+        return None
+    
+'''Parser Functions'''
+def pa_parser(logs):
+    delimiter = ','
+    parse = re.split(delimiter, logs)
+    # print(parse)
+    parsedlogs = list(filter(None, parse))
+    # print(parsedlogs)
+    for field in parsedlogs:
+        print(field)
+    return parsedlogs
+    
+def gws_parser(logs):
+    loads = json.loads(logs)
+    params = len(loads["events"][0])
+    print(params)
+    # Work to do: put if elif condition for the length of parameters. Google has different params length for different type of file owners e.g. shared drive, etc.
+    actoremail = loads["actor"]["email"]
+    action = loads["events"][0]["name"]
+    print(actoremail, action+"ed")
+    return loads
 
 
+def main():
+    user_input = main_menu_def()
+    if user_input == 's':
+        log_type = parser_menu_def(user_input)
+        if log_type == 'pa':
+            logs = parser_submenu_def(log_type)
+            if logs:
+                print(f"Log received: {logs}")
+                print('Processing...')
+                time.sleep(2)
+                pa_parser(logs)
+                return pa_parser
+        elif log_type == 'gws':
+            logs = parser_submenu_def(log_type)
+            if logs:
+                print(f"Log received: {logs}")
+                print()
+                print('Processing...')
+                time.sleep(2)
+                gws_parser(logs)
+                return gws_parser
+        elif log_type == 'gws':
+            logs = parser_submenu_def(log_type)
+            if logs:
+                print(f"Log received: {logs}")
+                print()
+                print('Processing...')
+                time.sleep(2)
+                pa_parser(logs)
+                return pa_parser
+    elif user_input == 'h':
+        parser_menu_def(user_input)
 
-# parsing = paloalto_traffic()
-# parsing_exec = menu_def(parsing, )
+# Run the main function
+main()
 
 
 
